2c62570f02ddd3e58038ddec9661bf5a1728f7fe
LorenCheng <loren@agreeinfotech.com>
2020-07-17 17:49:59 +0800

    add Payment:Created event listener

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { natsWrapper } from './nats-wrapper';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 import { ExpirationCompletedListener } from './events/listeners/expiration-completed-listener';
+import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const startApp = async () => {
   if (!process.env.JWT_SECRET_KEY) {
@@ -57,6 +58,7 @@ const startApp = async () => {
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
     new ExpirationCompletedListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
 
     // Connect to MongoDb
     await mongoose.connect(process.env.MONGO_URI, {
